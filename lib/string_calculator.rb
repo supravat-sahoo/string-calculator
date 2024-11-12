11da6ff6eee360c66a54a9d680a155f953c92309
Supravat <sahoosupravat8@gmail.com>
2024-11-12 21:00:55 +0530

    - Allow the add method to handle new lines between numbers (instead of commas)

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,6 +5,9 @@ class StringCalculator
     return 0 if str.empty?
 
     # Split the string by commas and convert each part to an integer
-    str.split(',').map(&:to_i).sum
+    # str.split(',').map(&:to_i).sum
+    
+    # Allow the add method to handle new lines between numbers (instead of commas). 
+    str.split(/,|\n/).map(&:to_i).sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,24 +3,32 @@ require './lib/string_calculator'
 RSpec.describe StringCalculator do
   let(:string_calculator) { StringCalculator.new }
 
-  it 'should get sum of comma separated numbers' do
-    expect(string_calculator.add('1,2,3')).to eq(6)
+  context 'add' do
+    it 'should get sum of comma separated numbers' do
+      expect(string_calculator.add("1,2,3")).to eq(6)
+    end
+
+    it 'should get sum as 0 for empty input' do
+      expect(string_calculator.add("")).to eq(0)
+    end
+
+    it 'should get sum for input without comma separated numbers' do
+      expect(string_calculator.add("12")).to eq(12)
+    end
+
+    it 'should get sum for large numbers' do
+      expect(string_calculator.add("1,2,3,8,8,10,7,8,10")).to eq(57)
+    end
+
+    it 'should get error for non string value' do
+      expect(string_calculator.add(1)).to eq('Input must be a string')
+    end
   end
 
-  it 'should get sum as 0 for empty input' do
-    expect(string_calculator.add('')).to eq(0)
-  end
-
-  it 'should get sum for input without comma separated numbers' do
-    expect(string_calculator.add('12')).to eq(12)
-  end
-
-  it 'should get sum for large numbers' do
-    expect(string_calculator.add('1,2,3,8,8,10,7,8,10')).to eq(57)
-  end
-
-  it 'should get error for non string value' do
-    expect(string_calculator.add(1)).to eq('Input must be a string')
+  context 'add with new line' do
+    it 'should get response for input with new line as well' do
+      expect(string_calculator.add("1\n2,3")).to eq(6)
+    end
   end
 
 end
